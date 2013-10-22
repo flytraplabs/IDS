aeefc1f512f78fcb142bdaa6180a4d9550b0f2b5
Marcello Duarte <marcello.duarte.dev@gmail.com>
2013-10-22 19:00:05 -0400

    fixes

--- setup.sh
+++ setup.sh
@@ -12,9 +12,9 @@ echo "masterauth $PASSWORD" >> /etc/redis/redis.conf
 sudo mkdir -p /opt/logstash
 wget https://download.elasticsearch.org/logstash/logstash/logstash-1.2.1-flatjar.jar -O /opt/logstash/logstash.jar
 wget https://raw.github.com/flytraplabs/IDS/master/indexer.conf -O /opt/logstash/indexer.conf
-sed -i 's/DEFAULT_PASSWORD/$PASSWORD/g' /opt/logstash/indexer.conf
+sed -i "s/DEFAULT_PASSWORD/$PASSWORD/g" /opt/logstash/indexer.conf
 sudo wget https://raw.github.com/flytraplabs/IDS/master/logstash-init.sh -O /etc/init.d/logstash-init.sh
 chmod +x /etc/init.d/logstash-init.sh
-mkdir -p /var/lock/subsys
+sudo mkdir -p /var/lock/subsys
 update-rc.d logstash-init.sh defaults
 initctl reload-configuration
